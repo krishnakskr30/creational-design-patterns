5e9552fe8781cca670667f4acb40aa1427d201eb
Karla Sai Krishna Reddy <krishnakskr333@gmail.com>
2022-10-25 12:15:52 +0530

    Add object pool design pattern

---
+++ src/main/java/objectpool/ObjectPool.java
@@ -0,0 +1,37 @@
+package objectpool;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.function.Supplier;
+
+public class ObjectPool<T extends Poolable> {
+
+    //In memory cache
+    private final BlockingQueue<T> availablePool;
+
+    public ObjectPool(Supplier<T> creator, int count) {
+        availablePool = new LinkedBlockingQueue<>();
+        for (int i = 0; i < count; i++) {
+            availablePool.offer(creator.get());
+        }
+    }
+
+    //here we are just taking out the object and indefinitely waiting
+    public T get() {
+        try {
+            return availablePool.take();
+        } catch (InterruptedException ex) {
+            System.err.println("take() was interrupted");
+        }
+        return null;
+    }
+
+    public void release(T obj) {
+        obj.reset();
+        try {
+            availablePool.put(obj);
+        } catch (InterruptedException e) {
+            System.err.println("put() was interrupted");
+        }
+    }
+}
